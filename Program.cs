ba30a514a783dc8db6ee5362aa9a3dd2cd3b3f67
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-08-22 11:16:23 +0200

    5. Data Types

--- Program.cs
+++ Program.cs
@@ -29,6 +29,16 @@ namespace c_sharp_try
             Console.WriteLine("He really liked the name " + characterName);
             Console.WriteLine("But didn't like being " + characterAge);
 
+            // 5. Data Types
+
+            string phrase = "Hi!";
+            char grade = 'C';
+            int age = 30;
+            double gpa = 3.2;
+            // float gpa = 3.2;
+            // decimal gpa = 3.2;
+            bool isMale = true;
+
             Console.ReadLine();
         }
     }
