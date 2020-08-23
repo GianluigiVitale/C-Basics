4de7ecfc4b7735713b8db1276d87941246623ecf
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-08-23 14:38:51 +0200

    12. Methods

--- Program.cs
+++ Program.cs
@@ -142,7 +142,18 @@ namespace c_sharp_try
             Console.WriteLine(luckyNumbers[1]);
             Console.WriteLine(luckyNumbers[2]);
 
+
+            // 12. Methods
+
+
+            SayHi("Mike", 18);
+
             Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age);
         }
     }
 }
